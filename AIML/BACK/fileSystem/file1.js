607bf5374950c5ee94788a8e38819e5968af52fc
Aditya Singh <sighaditya25@gmail.com>
2025-02-07 09:52:46 +0530

    UPdated work 7/02/2025

--- AIML/BACK/fileSystem/file1.js
+++ AIML/BACK/fileSystem/file1.js
@@ -1,3 +1,19 @@
+//const fs = require('fs');
+//const data = fs.readFileSync("./data.txt","utf-8");
+//console.log(data);
+
+//fs.appendFileSync("./data1.txt", " work from the another file","utf-8");
+//fs.renameSync("./data3.txt", "./data5.txt");
+//console.log(data);
+//fs.unlinkSync("./data5.txt")
+
+
 const fs = require('fs');
 const data = fs.readFileSync("./data.txt","utf-8");
 console.log(data);
+if(data.match("H"))
+{
+    const newData = data.replace("H", "ABES");
+    fs.writeFileSync("./data.txt", newData, "utf-8")
+}
+
